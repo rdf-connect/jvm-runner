2fdd355886ae5bd4d14ec6476030ed7d23e419af
ajuvercr <arthur.vercruysse@outlook.com>
2025-08-19 22:12:35 +0200

    bootstrap fetch the jar

--- runner/src/main/java/io/github/rdfc/Runner.java
+++ runner/src/main/java/io/github/rdfc/Runner.java
@@ -10,7 +10,6 @@ import java.util.List;
 import java.util.Optional;
 import java.util.function.Consumer;
 
-import io.github.rdfc.Rdfc;
 import io.github.rdfc.json.ChannelHandlerModule;
 
 import com.fasterxml.jackson.databind.DeserializationFeature;
